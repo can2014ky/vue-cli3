908d58da03893a46aeeb307d9acecf775bcbed83
zhoucan1 <zhoucan1@360.cn>
2020-04-17 16:21:02 +0800

    v-jsoneditor

--- src/views/dashbord/Dash1.vue
+++ src/views/dashbord/Dash1.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
     <div>Welcome to GOSINT!111</div>
+    <v-jsoneditor v-model="json" :options="options" height="400px"></v-jsoneditor>
   </div>
 </template>
 <script>
+// https://github.com/josdejong/jsoneditor/blob/master/docs/api.md#configuration-options?blank
+import VJsoneditor from 'v-jsoneditor'
 export default {
-
+  components: {
+    VJsoneditor
+  },
+  data() {
+    return {
+      json: {
+        'hello': 'vue'
+      },
+      options: {
+        mainMenuBar: false,
+        mode: 'code',
+        onEditable: ()=> {}
+      }
+    }
+  }
 }
 </script>
