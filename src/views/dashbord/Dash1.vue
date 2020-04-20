d9c9dfb1ab60ea5ae6eb07a0af6213643c2917e7
zhoucan1 <zhoucan1@360.cn>
2020-04-20 18:07:28 +0800

    v-jsoneditor  配置

--- src/views/dashbord/Dash1.vue
+++ src/views/dashbord/Dash1.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>Welcome to GOSINT!111</div>
-    <v-jsoneditor v-model="json" :options="options" height="400px"></v-jsoneditor>
+    <v-jsoneditor v-model="json" :options="options" :plus="false" width="100px" height="400px"></v-jsoneditor>
   </div>
 </template>
 <script>
@@ -14,7 +14,37 @@ export default {
   data() {
     return {
       json: {
-        'hello': 'vue'
+        'hello': 'vue',
+        info: {
+          name: 'lily',
+          age: '18',
+          sex: 'male'
+        },
+        info1: {
+          name: 'lily',
+          age: '18',
+          sex: 'male'
+        },
+        info2: {
+          name: 'lily',
+          age: '18',
+          sex: 'male'
+        },
+        info3: {
+          name: 'lily',
+          age: '18',
+          sex: 'male'
+        },
+        info4: {
+          name: 'lily',
+          age: '18',
+          sex: 'male',
+          info6: {
+            name: 'lily',
+            age: '18',
+            sex: 'male'
+          }
+        }
       },
       options: {
         mainMenuBar: false,
